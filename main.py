d5abe4ac46d171f9371eef1b40ba88f308c1c28b
Andreas Eliasson <andreaseliasson91@gmail.com>
2021-09-30 12:00:35 +0200

    Lunch Commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
-    with open("./TransactionFiles/travel.txt", "r", encoding="utf-8") as file:
+    # file_name = input("Enter the name of the file: ")
+    file_name = "travel.txt"
+
+    with open("./TransactionFiles/" + file_name, "r", encoding="utf-8") as file:
         t = file.readlines()
-        print(t)
+
+    print("Date\tType\tName\tAmount\tPersons\tNames")
+    for i in t:
+        print(i)
 
 
 if __name__ == "__main__":
